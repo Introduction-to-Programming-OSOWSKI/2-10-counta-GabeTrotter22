2f36b2c35b4d6748f74e5af6dd35993ec32d2e2a
Your Name <827511@mystma.org>
2021-10-06 14:59:05 -0500

    2

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ def countA(w):
     total = 0
 
     for i in range(0, len(w)):
-        total = total * i
-        if w[0] == "a":
+        if w[i] == "a":
             return total
 
-countA("alphabet")
+countA("rat")
 
