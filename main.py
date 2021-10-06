0e3326009c3346223957dab3a8815d4ed52e5170
Your Name <827511@mystma.org>
2021-10-06 14:54:43 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    total = 0
+
+    for i in range(0, len(w)):
+        total = total * i
+        if w[0] == "a":
+            return total
+
+countA("alphabet")
+
